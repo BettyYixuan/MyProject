7b2b55bbe8c3408cd0da2107ded5f7830bcefd5c
Betty <u3629195@connect.hku.hk>
2025-03-05 13:57:49 +0800

    feat: guessing number

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
+import random
 print("Hello")
+
+answer = random.randint(1,5)
+guess = int(input("Please enter an integer: "))
+while (answer != guess):
+    print("You are wrong.")
+    guess = int(input("Please enter another integer:"))
+print("You are right!")
